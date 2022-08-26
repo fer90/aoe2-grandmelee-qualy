17f6cc5c9a1b429bc4357883649dd6c5fd342f95
Bruno Galeotti <bgaleotti@gmail.com>
2022-08-26 13:45:53 -0300

    add flags

--- src/tournament/SingleBracket.js
+++ src/tournament/SingleBracket.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Flag from 'react-world-flags';
 
-function Player({ position, name, rating }) {
+function Player({ country, position, name, rating }) {
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '30px 1fr 150px', }}>
+    <div style={{ display: 'grid', gridGap: '8px', gridTemplateColumns: '30px 30px 1fr 150px' }}>
       <div>{position}.</div>
+      <Flag code={country} />
       <div><strong>{name}</strong></div>
       <div>(<strong>{rating.highest}</strong> - [{rating.current}])</div>
     </div>
@@ -12,9 +14,9 @@ function Player({ position, name, rating }) {
 
 export default function SingleBracket({ playerOne, playerOnePosition, playerTwo, playerTwoPosition }) {
     return (
-        <div style={{ border: '1px solid #fff', display: 'flex', flexDirection: 'column', padding: '8px' }}>
-            <Player position={playerOnePosition} name={playerOne.name} rating={{ current: playerOne.rating, highest: playerOne.highest_rating }} />
-            <Player position={playerTwoPosition} name={playerTwo.name} rating={{ current: playerTwo.rating, highest: playerTwo.highest_rating }} />
+        <div style={{ border: '1px solid #fff', display: 'flex', flexDirection: 'column', gap: '8px', padding: '8px' }}>
+            <Player position={playerOnePosition} name={playerOne.name} rating={{ current: playerOne.rating, highest: playerOne.highest_rating }} country={playerOne.country} />
+            <Player position={playerTwoPosition} name={playerTwo.name} rating={{ current: playerTwo.rating, highest: playerTwo.highest_rating }} country={playerTwo.country} />
         </div>
     );
 }
